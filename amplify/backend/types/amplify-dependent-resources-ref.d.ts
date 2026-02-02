952c4573343e8bd88fc4d6fd424f2081aabf62e3
rvillanueva77 <ric.villanueva@gmail.com>
2026-02-02 00:08:14 -0300

    petstore 02feb2026

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "petstore": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
